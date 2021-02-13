fd8cd95d930b4b42be8508bf00aec45fdbf93961
Sean <seanpatrickllera@yahoo.ca>
2021-02-13 18:04:19 -0500

    Added buttons and text options, still implementing clicking response

--- pyxel-master/pyxel/sunset.py
+++ pyxel-master/pyxel/sunset.py
@@ -1,26 +1,48 @@
 import pyxel
 
+class Option:
+    def __init__(self, x, y, side, color):
+        self.x = x
+        self.y = y
+        self.side = side
+        self.color = color
+
 class App:
     def __init__(self):
         pyxel.init(256, 256, caption="Evening", fullscreen=True)
         pyxel.image(0).load(0, 0, "assets/sunset.png")
         pyxel.mouse(True)
 
+        self.option1 = Option(2,232,5,12)
+        self.option2 = Option(2,240,5,12)
+        self.option3 = Option(2,248,5,12)
+        
         # EVERYTHING MUST BE ABOVE THIS LINE
         pyxel.run(self.update, self.draw)
         
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+        if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
+            self.option3.color = 0
+            if(pyxel.mouse_x > self.option1.x & pyxel.mouse_x < (self.option1.x + self.option1.side) & pyxel.mouse_y > self.option1.y & pyxel.mouse_y < (self.option1.y + self.option1.side)):
+                self.option1.color = 0
 
     def draw(self):
         pyxel.cls(0)
         pyxel.blt(0, 0, 0, 0, 0, 256, 256)
         # rect(x, y, w, h, col)
         pyxel.rect(0, 180, 256, 76, 0)
-        pyxel.text(2, 182, "There is text here gotem", pyxel.frame_count % 16)
-        pyxel.text(2, 189, "There is text here too very cool stuff very nice", pyxel.frame_count % 16)
+        pyxel.text(2, 182, "There is text here gotem", 3)
+        pyxel.text(2, 189, "There is text here too very cool stuff very nice", 4)
         self.test_text(2, 200)
+        # pyxel.text(2, 232, str(self.option1.y), pyxel.frame_count % 16)
+        pyxel.rect(self.option1.x, self.option1.y, self.option1.side, self.option1.side, self.option1.color)
+        pyxel.text(10, self.option1.y, "This is option 1", 2)
+        pyxel.rect(self.option2.x, self.option2.y, self.option2.side, self.option2.side, self.option2.color)
+        pyxel.text(10, self.option2.y, "This is option 2", 8)
+        pyxel.rect(self.option3.x, self.option3.y, self.option3.side, self.option3.side, self.option3.color)
+        pyxel.text(10, self.option3.y, "This is option 3", 9)
 
     def test_text(self, x, y):
         pyxel.text(x, y, "text(x,y,s,col)", 7)
@@ -33,5 +55,6 @@ class App:
 
         pyxel.text(x + 1, y, s, 1)
         pyxel.text(x, y, s, 9)
+        # pyxel.rect(0, 20, 20, 20, pyxel.frame_count % 16)        
 
 App()
