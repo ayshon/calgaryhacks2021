613bcc1a33bc3c843d3268f54fef14a637dfbac4
Sean <seanpatrickllera@yahoo.ca>
2021-02-13 18:48:29 -0500

    Implemented background change when option buttons are clicked

--- pyxel-master/pyxel/sunset.py
+++ pyxel-master/pyxel/sunset.py
@@ -11,12 +11,15 @@ class App:
     def __init__(self):
         pyxel.init(256, 256, caption="Evening", fullscreen=True)
         pyxel.image(0).load(0, 0, "assets/sunset.png")
+        
         pyxel.mouse(True)
 
         self.option1 = Option(2,232,5,12)
         self.option2 = Option(2,240,5,12)
         self.option3 = Option(2,248,5,12)
         
+        self.activeBank = 0
+
         # EVERYTHING MUST BE ABOVE THIS LINE
         pyxel.run(self.update, self.draw)
         
@@ -27,13 +30,17 @@ class App:
             if(pyxel.mouse_x > self.option1.x and pyxel.mouse_x < (self.option1.x + self.option1.side) and pyxel.mouse_y > self.option1.y and pyxel.mouse_y < (self.option1.y + self.option1.side)):
                 if(self.option1.color == 12):
                     self.option1.color = 0
+                    pyxel.image(0).load(0, 0, "assets/forest.png")
                 else:
                     self.option1.color = 12
+                    pyxel.image(0).load(0, 0, "assets/sunset.png")
             if(pyxel.mouse_x > self.option2.x and pyxel.mouse_x < (self.option2.x + self.option2.side) and pyxel.mouse_y > self.option2.y and pyxel.mouse_y < (self.option2.y + self.option2.side)):
                 if(self.option2.color == 12):
                     self.option2.color = 0
+                    pyxel.image(0).load(0, 0, "assets/lake.png")
                 else:
                     self.option2.color = 12
+                    pyxel.image(0).load(0, 0, "assets/sunset.png")
             if(pyxel.mouse_x > self.option3.x and pyxel.mouse_x < (self.option3.x + self.option3.side) and pyxel.mouse_y > self.option3.y and pyxel.mouse_y < (self.option3.y + self.option3.side)):
                 if(self.option3.color == 12):
                     self.option3.color = 0
@@ -41,13 +48,23 @@ class App:
                     self.option3.color = 12
 
     def draw(self):
+        # CLEAR SCREEN
         pyxel.cls(0)
+
+        # BACKGROUND
+        # blt(x, y, img, u, v, w, h, [colkey]) colkey is optional
         pyxel.blt(0, 0, 0, 0, 0, 256, 256)
+
+        # DRAW BLACK TEXTBOX
         # rect(x, y, w, h, col)
         pyxel.rect(0, 180, 256, 76, 0)
+
+        # TEXT
         pyxel.text(2, 182, "There is text here gotem", 3)
         pyxel.text(2, 189, "There is text here too very cool stuff very nice", 4)
         self.test_text(2, 200)
+
+        # OPTIONS
         # pyxel.text(2, 232, str(self.option1.y), pyxel.frame_count % 16)
         pyxel.rect(self.option1.x, self.option1.y, self.option1.side, self.option1.side, self.option1.color)
         pyxel.text(10, self.option1.y, "This is option 1", 2)
