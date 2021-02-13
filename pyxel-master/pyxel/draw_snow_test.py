27a90dd042c3991ff18d48be1d70db53c3cf1a6b
Priscilla Deng <priscilla.deng@mail.utoronto.ca>
2021-02-13 16:32:20 -0500

    Image drawing partly working, see draw_snow.py

---
+++ pyxel-master/pyxel/draw_snow_test.py
@@ -0,0 +1,38 @@
+from random import randint
+
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120, caption="Pyxel Jump")
+
+        pyxel.load("assets/snow.pyxres")
+        self.score = 0
+        self.player_x = 72
+        self.player_y = -16
+        self.player_vy = 0
+        self.player_is_alive = True
+
+        self.far_cloud = [(-10, 75), (40, 65), (90, 60)]
+        self.near_cloud = [(10, 25), (70, 35), (120, 15)]
+        self.floor = [(i * 60, randint(8, 104), True) for i in range(4)]
+        self.fruit = [(i * 60, randint(0, 104), randint(0, 2), True) for i in range(4)]
+
+        pyxel.playm(0, loop=True)
+
+        pyxel.run(self.update, self.draw)
+    
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+
+    def draw(self):
+        pyxel.cls(12)
+
+        # draw sky
+        pyxel.blt(0, 0, 0, 0, 0, 248, 248)
+
+
+App()
