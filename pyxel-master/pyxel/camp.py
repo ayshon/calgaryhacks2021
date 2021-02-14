84cfc3000e7022bed6fefea4d986524cf449c525
Priscilla Deng <priscilla.deng@mail.utoronto.ca>
2021-02-13 22:50:36 -0500

    Updated inventory changes for camp.py

--- pyxel-master/pyxel/camp.py
+++ pyxel-master/pyxel/camp.py
@@ -17,7 +17,9 @@ class App:
         
         self.activeBank = 0
         self.gender = 0
-        self.inventory = ["Canadian Bacon"]
+        self.inventory = [["Canadian Bacon", 2], ["Dummy", 1]]
+        self.status = 0  # ENSURES INVENTORY IS ONLY UPDATED ONCE
+        self.consumed = 0 # WHAT WAS EATEN
 
         # EVERYTHING MUST BE ABOVE THIS LINE
         pyxel.run(self.update, self.draw)
@@ -39,10 +41,22 @@ class App:
         pyxel.rect(0, 180, 256, 76, 0)
 
         # TEXT
-        pyxel.text(2, 192, "You have decided to make camp and rest for the next day.", 7)
-        pyxel.text(2, 202, "You have decided to eat one " + self.inventory[0] + ".", 7)
+        if (self.status < 1):
+            self.consumed = self.inventory[0][0]
 
-        pyxel.text(2, 232, "Click anywhere to continue.", 7)
+        pyxel.text(2, 188, "You have decided to make camp and rest for the next day.", 7)
+        pyxel.text(2, 198, "You have decided to eat one " + self.consumed + ".", 7)
+
+        pyxel.text(2, 212, "Changes in inventory:", 7)
+        pyxel.text(2, 222, "-" + self.consumed + "      -1", 7)
+
+        pyxel.text(2, 238, "Click anywhere to continue.", 7)
         # NOTE: REQUIRES CODE TO TRANSITION INTO NEXT SCENE
+
+        if(self.status < 1):
+            self.inventory[0][1] -= 1
+            if (self.inventory[0][1] == 0):
+                self.inventory.pop(0)
+        self.status = 1
 
 App()
